df1d5f0ac9f648fd7926df2a2ebb0020604e8ee3
chris.e.richardson <chris.e.richardson@96aafd2f-ff2b-0410-b1e4-c16dfad04aa2>
2007-04-16 20:30:05 +0000

    Switched to standard environment selection mechanism

--- trunk/webapp/src/test/java/org/jia/ptrack/webapp/test/RunDetachedObjectsWebApplication.java
+++ trunk/webapp/src/test/java/org/jia/ptrack/webapp/test/RunDetachedObjectsWebApplication.java
@@ -14,19 +14,14 @@ public class RunDetachedObjectsWebApplication extends RunWebApplication {
 	}
 
 	protected void extraMunging() {
-		System
-				.setProperty("session.factory",
-						"/appCtxForDetachedObjects/hibernate/eager-session-factory.xml");
-
-		replace(TARGET_WEB_XML, "classpath:/appCtx/hibernate/repositories.xml",
-				"classpath:/appCtxForDetachedObjects/hibernate/eager-repositories.xml");
-		
 		// TODO It would be nice to verify that this succeeded
-		
+
 		replace(
 				TARGET_WEB_XML,
 				"<filter-mapping><filter-name>OpenSessionInViewFilter</filter-name><url-pattern>/*</url-pattern></filter-mapping>",
 				"");
+		replace(TARGET_WEB_XML, "classpath*:/appCtx/**/*.xml",
+				"classpath*:/appCtx/**/*.xml classpath*:/appCtxForDetachedObjects/**/*.xml");
 	}
 
 	protected void replace(String file, String fromString, String toString) {
@@ -34,6 +29,7 @@ public class RunDetachedObjectsWebApplication extends RunWebApplication {
 		replaceTask.setFile(new File(file));
 		replaceTask.setToken(fromString);
 		replaceTask.setValue(toString);
+		replaceTask.setSummary(true);
 		replaceTask.execute();
 	}
 
